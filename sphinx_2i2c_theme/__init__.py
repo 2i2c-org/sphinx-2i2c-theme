80cc057d077024b35fd9c079c640236f7d137aba
Chris Holdgraf <choldgraf@berkeley.edu>
2021-10-01 08:53:21 -0700

    Updating theme a bit

--- sphinx_2i2c_theme/__init__.py
+++ sphinx_2i2c_theme/__init__.py
@@ -8,3 +8,4 @@ def setup(app):
     path_theme = os.path.abspath(Path(__file__).parent)
     app.add_html_theme("sphinx_2i2c_theme", path_theme)
     app.add_css_file("https://code.cdn.mozilla.net/fonts/fira.css")
+    app.config.html_favicon = "https://2i2c.org/media/icon.png"
