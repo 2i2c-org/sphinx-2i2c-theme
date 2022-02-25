d6af1d374c000d006b20b938279c0abc22b86461
Chris Holdgraf <choldgraf@berkeley.edu>
2022-02-25 10:24:26 -0800

    Fixing theme SCSS and logic to work with new book theme (#7)

--- src/sphinx_2i2c_theme/__init__.py
+++ src/sphinx_2i2c_theme/__init__.py
@@ -1,12 +1,18 @@
 """A lightweight theme for 2i2c."""
 import os
 from pathlib import Path
+from sphinx_book_theme import hash_assets_for_files
 
 __version__ = "0.0.1"
 
 THEME_PATH = (Path(__file__).parent / "theme" / "sphinx-2i2c-theme").resolve()
 
+def hash_html_assets(app, pagename, templatename, context, doctree):
+    assets = ["styles/sphinx-2i2c-theme.css"]
+    hash_assets_for_files(assets, THEME_PATH / "static", context)
+
 def setup(app):
     app.add_html_theme("sphinx_2i2c_theme", THEME_PATH)
     app.add_css_file("https://code.cdn.mozilla.net/fonts/fira.css")
     app.config.html_favicon = "https://2i2c.org/media/icon.png"
+    app.connect("html-page-context", hash_html_assets)
